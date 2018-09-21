40962101301de5d8eaf9c6a3077e8c9d372afcf4
payaln <payaln@mail.ru>
2018-09-21 16:06:34 +0300

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <math.h>
+
+int main() {
+
+    char c='j';
+    int a=75;
+    std::cout<<c/float(a)<<'\n';
+    std::cout<<c*a<<'\n';
+    std::cout<<c+a<<'\n';
+
+    return 0;
+}
